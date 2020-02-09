7961f368188a82001f2659031bcdc9ca0761bd09
VojinDev <56448507+VojinDev@users.noreply.github.com>
2020-02-09 14:23:40 +0100

    Started work on writing to files and polished cd command

--- GOS/Kernel.cs
+++ GOS/Kernel.cs
@@ -13,7 +13,6 @@ namespace GOS
         ConsoleColor themeColor4;
         int currentTheme;
 
-        bool applicationMode = false;
         protected override void BeforeRun()
         {
             var fs = new Sys.FileSystem.CosmosVFS();
@@ -40,156 +39,212 @@ namespace GOS
 
         protected override void Run()
         {
-                inputText();
-                var input = Console.ReadLine();
-                string cmd = input.Split(" ")[0];
-                switch (cmd)
-                {
-                    case "shutdown":
-                        Sys.Power.Shutdown();
-                        break;
+            inputText();
+            var input = Console.ReadLine();
+            string cmd = input.Split(" ")[0];
+            switch (cmd)
+            {
+                case "shutdown":
+                    Sys.Power.Shutdown();
+                    break;
 
-                    case "reboot":
-                        Sys.Power.Reboot();
-                        break;
-                    
-                    case "ls":
-                        string[] dirDirectories = Directory.GetDirectories(Directory.GetCurrentDirectory());
-                        foreach (var dir in dirDirectories)
-                        {
-                            Console.WriteLine(dir + " | Directory");
-                        }
-                        string[] dirFiles = Directory.GetFiles(Directory.GetCurrentDirectory());
-                        foreach (var file in dirFiles)
-                        {
-                            Console.WriteLine(file + " | File");
-                        }
-                        break;
+                case "reboot":
+                    Sys.Power.Reboot();
+                    break;
 
-                    case "cd":
-                        var targetDir = input.Remove(0, input.IndexOf(' ') + 1);
-                        if (Directory.Exists(targetDir))
-                        {
-                            Directory.SetCurrentDirectory(targetDir);
-                        }
-                        break;
+                case "ls":
+                    string[] dirDirectories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+                    foreach (var dir in dirDirectories)
+                    {
+                        Console.WriteLine(dir + " | Directory");
+                    }
+                    string[] dirFiles = Directory.GetFiles(Directory.GetCurrentDirectory());
+                    foreach (var file in dirFiles)
+                    {
+                        Console.WriteLine(file + " | File");
+                    }
+                    break;
 
-                    case "beep":
-                        var targetFreqRaw = input.Remove(0, input.IndexOf(' ') + 1);
-                        int targetFreq = int.Parse(targetFreqRaw);
-                        if (targetFreq < 37 || targetFreq > 32767)
+                case "cd":
+                    if (input.Contains(" "))
+                    {
+                        if (!input.EndsWith(" "))
                         {
-                            error("Frequency cant be less than 37hz or larger than 32767hz");
-                        }
-                        else
-                        {
-                            Console.Beep(targetFreq, 1000);
-                        }
-                        break;
-
-                    case "cat":
-                        var targetCat = input.Remove(0, input.IndexOf(' ') + 1);
-                        if (!File.Exists(targetCat))
-                        {
-                            Console.WriteLine("Could not find file");
-                            break;
-                        }
-                        string[] lines = File.ReadAllLines(targetCat);
-
-                        foreach (string line in lines)
-                        {
-                            Console.WriteLine(line);
-                        }
-                        break;
-
-                    case "grep":
-                        List<string> flags = new List<string>();
-                        foreach (string x in input.Split(" "))
-                        {
-                            flags.Add(x);
-                        }
-                        if (flags.Count > 1)
-                            if (flags[1] == "-h")
+                            var targetDir = input.Split(" ")[1];
+                            if (Directory.Exists(targetDir))
                             {
-                                Console.WriteLine("GOS implementation of grep\nUsage: grep PATTERN FILENAME\n\nOptional switches:\n    -c Count each PATTERN instance\n    -w force PATTERN to match only whole words");
-                                break;
+                                if (!targetDir.Contains("0:\\"))
+                                {
+                                    if (!Directory.GetCurrentDirectory().EndsWith("\\"))
+                                    {
+                                        Directory.SetCurrentDirectory(Directory.GetCurrentDirectory() + "\\" + targetDir);
+                                    }
+                                    else
+                                    {
+                                        Directory.SetCurrentDirectory(Directory.GetCurrentDirectory() + targetDir);
+                                    }
+                                }
+                                else
+                                {
+                                    Directory.SetCurrentDirectory(targetDir);
+                                }
+
                             }
+                            else
+                            {
+                                error(targetDir + " Isn't recognized as a directory");
+                            }
+                        }
+                        else
+                        {
+                            error("No path in argument. Usage: cd 'path'");
+                        }
+                    }
+                    else
+                    {
+                        Directory.SetCurrentDirectory("0:\\");
+                    }
+                    break; 
+
+                case "beep":
+                    var targetFreqRaw = input.Remove(0, input.IndexOf(' ') + 1);
+                    int targetFreq = int.Parse(targetFreqRaw);
+                    if (targetFreq < 37 || targetFreq > 32767)
+                    {
+                        error("Frequency cant be less than 37hz or larger than 32767hz");
+                    }
+                    else
+                    {
+                        Console.Beep(targetFreq, 1000);
+                    }
+                    break;
+
+                case "cat":
+                    var targetCat = input.Remove(0, input.IndexOf(' ') + 1);
+                    if (!File.Exists(targetCat))
+                    {
+                        Console.WriteLine("Could not find file");
+                        break;
+                    }
+                    string[] lines = File.ReadAllLines(targetCat);
+
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    break;
+
+                case "grep":
+                    List<string> flags = new List<string>();
+                    foreach (string x in input.Split(" "))
+                    {
+                        flags.Add(x);
+                    }
+                    if (flags.Count > 1)
+                        if (flags[1] == "-h")
+                        {
+                            Console.WriteLine("gOS implementation of grep\nUsage: grep PATTERN FILENAME\n\nOptional switches:\n    -c Count each PATTERN instance\n    -w force PATTERN to match only whole words");
+                            break;
+                        }
+
+                    if (flags.Count < 3)
+                    {
+                        Console.WriteLine("Too few arguments, excpected at least 2");
+                        break;
+                    }
+                    Grep gp = new Grep();
+                    gp.grep(flags[2], flags[1], flags);
+                    break;
+
+                case "mkdir":
+                    var dirPath = input.Remove(0, input.IndexOf(' ') + 1);
+                    Directory.CreateDirectory(dirPath);
+                    break;
+
+                case "touch":
+                    var touchPath = input.Remove(0, input.IndexOf(' ') + 1);
+                    File.Create(touchPath);
+                    break;
+
+                case "write":
+                    var writePath = input.Split(" ")[1];
+                    var writeText = input.Split(" ")[2];
+                    if (File.Exists(writePath))
+                    {
+                        File.AppendAllText(writePath, writeText);
+                    }
+                    break;
+
+                case "writeline":
+                    var writelinePath = input.Split(" ")[1];
+                    var writelineText = input.Split(" ")[2];
+                    if (File.Exists(writelinePath))
+                    {
+                        File.AppendAllText(writelinePath, Environment.NewLine + writelineText);
+                    }
+                    break;
+
+                case "rm":
+                    var delTarget = input.Remove(0, input.IndexOf(' ') + 1);
+                    if (File.Exists(delTarget))
+                    {
+                        File.Delete(delTarget);
+                    }
+                    else if (Directory.Exists(delTarget))
+                    {
+                        Directory.Delete(delTarget);
+                    }
+                    else
+                    {
+                        error("File or Directory does not exist / isn't valid");
+                    }
+                    break;
+
+                case "clear":
+                    Console.Clear();
+                    break;
+
+                case "reinit":
+                    OSinit();
+                    break;
+
+                case "echo":
+                    var echoMsg = input.Remove(0, input.IndexOf(' ') + 1);
+                    Console.WriteLine(echoMsg);
+                    break;
+
+                case "run":
+                    var targetApplication = input.Remove(0, input.IndexOf(' ') + 1);
+                    switch (targetApplication)
+                    {
                         
-                        if (flags.Count < 3)
-                        {
-                            Console.WriteLine("Too few arguments, excpected at least 2");
-                            break;
-                        }
-                        Grep gp = new Grep();
-                        gp.grep(flags[2], flags[1], flags);
+                    }
+                    break;
+
+                case "theme":
+                    var targetTheme = input.Remove(0, input.IndexOf(' ') + 1);
+                    string[] theme_names = { "Ice (Default)", "Plain", "Inferno", "Sahara", "Magic", "Indian Hacker" };
+                    Int32.TryParse(targetTheme, out currentTheme);
+
+                    if ((currentTheme < 1) || (currentTheme > 6))
+                    {
+                        currentTheme = 1;
+                        error(targetTheme + " is not recognized as a theme");
                         break;
+                    }
 
-                    case "mkdir":
-                        var dirPath = input.Remove(0, input.IndexOf(' ') + 1);
-                        Directory.CreateDirectory(dirPath);
+                    lineChanger("theme = " + currentTheme, "0:\\settings.ini", 1);
+                    Console.WriteLine("Switching to " + theme_names[currentTheme - 1] + " theme");
+                    break;
+
+                default:
+                    if (string.IsNullOrWhiteSpace(input))
                         break;
+                    else
+                        error(cmd + " is not recognized as a command");
+                    break;
+            }
 
-                    case "rm":
-                        var delTarget = input.Remove(0, input.IndexOf(' ') + 1);
-                        if (File.Exists(delTarget))
-                        {
-                            File.Delete(delTarget);
-                        }
-                        else if (Directory.Exists(delTarget))
-                        {
-                            Directory.Delete(delTarget);
-                        }
-                        else
-                        {
-                            error("File or Directory does not exist / isn't valid");
-                        }
-                        break;
-
-                    case "clear":
-                        Console.Clear();
-                        break;
-
-                    case "reinit":
-                        OSinit();
-                        break;
-
-                    case "echo":
-                        var echoMsg = input.Remove(0, input.IndexOf(' ') + 1);
-                        Console.WriteLine(echoMsg);
-                        break;
-
-                    case "run":
-                        var targetApplication = input.Remove(0, input.IndexOf(' ') + 1);
-                        switch(targetApplication)
-                        {
-                           
-                        }
-                        break;
-
-                    case "theme":
-                        var targetTheme = input.Remove(0, input.IndexOf(' ') + 1);
-                        string[] theme_names = { "Ice (Default)", "Plain", "Inferno", "Sahara", "Magic", "Indian Hacker" };
-                        Int32.TryParse(targetTheme, out currentTheme);
-
-                        if ((currentTheme < 1) || (currentTheme > 6))
-                        {
-                            currentTheme = 1;
-                            error(targetTheme + " is not recognized as a theme");
-                            break;
-                        }
-
-                        lineChanger("theme = " + currentTheme, "0:\\settings.ini", 1);
-                        Console.WriteLine("Switching to " + theme_names[currentTheme-1] + " theme");
-                        break;
-
-                    default:
-                        if (string.IsNullOrWhiteSpace(input))
-                            break;
-                        else
-                            error(cmd + " is not recognized as a command");
-                        break;
-                }
-         
         }
 
         protected void inputText()
@@ -227,13 +282,14 @@ namespace GOS
             Console.Clear();
             ColorSettings(currentTheme);
             Console.ForegroundColor = themeColor1;
-            Console.WriteLine("  ____  ___  ____  ");
-            Console.WriteLine(" / ___|/ _ \\/ ___| ");
+            Console.WriteLine("          ____  _____");
+            Console.WriteLine("   ______/ __ \\/ ___/");
             Console.ForegroundColor = themeColor2;
-            Console.WriteLine("| |  _| | | \\___ \\ ");
-            Console.WriteLine("| |_| | |_| |___) |");
+            Console.WriteLine("  / __  / / / /\\__ \\ ");
+            Console.WriteLine(" / /_/ / /_/ /___/ / ");
             Console.ForegroundColor = themeColor3;
-            Console.WriteLine(" \\____|\\___/|____/ ");
+            Console.WriteLine(" \\___ /\\____//____/  ");
+            Console.WriteLine("/____/               ");
         }
 
         static void lineChanger(string newText, string fileName, int line_to_edit)
